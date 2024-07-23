61ccbb925383b69bc59262227bb26b06dc17fec1
Tanisha Jain <itanishajain@gmail.com>
2024-07-23 22:17:08 +0530

    STL-List

--- STL-list.cpp
+++ STL-list.cpp
@@ -15,14 +15,29 @@ void display(list<int> &l){
 
 int main(){
     list<int> l1; // List of 0 length.
-    list<int> l2(3); //List of size 7.
-
+    
     l1.push_back(67);
-    l1.push_back(4);
+    l1.push_back(54);
     l1.push_back(5);
     l1.push_back(78);
     l1.push_back(24);
+    display(l1);
+    //l1.pop_back(); // It will remove element from the back/end of the list.
+    //l1.pop_front(); // It will remove element from fromt of the list
+    //l1.remove(24); // it will remove element which is specified.
 
+    // To sort the list
+    l1.sort();
+
+    display(l1);
+    
+    // To reverse the list
+    l1.reverse();
+
+    display(l1);
+
+    
+    list<int> l2(3); //List of size 7.
     list<int> :: iterator iter;
     iter = l2.begin();
     *iter = 34;
@@ -32,8 +47,9 @@ int main(){
     *iter = 58;
     iter++;
     display(l2);
-
-    display(l1);
     
+    l1.merge(l2);
+    cout << "List 1 after merging with list 2 -> " ;
+    display(l1);
     return 0;
 }
